b18aa67aa3faaa0c603329f4c91e758e554596a8
Arne Groskurth <arne.groskurth@gears9.de>
2017-12-08 23:39:24 +0100

    Added NO_DUTY_REQUEST event type and simplified all event type names

--- src/event-types.js
+++ src/event-types.js
@@ -1,12 +1,14 @@
 
-export const EVENT_TYPE_VACATION = 'EVENT_TYPE_VACATION';
-export const EVENT_TYPE_DUTY = 'EVENT_TYPE_DUTY';
-export const EVENT_TYPE_WARD = 'EVENT_TYPE_WARD';
-export const EVENT_TYPE_MISC = 'EVENT_TYPE_MISC';
+export const EVENT_TYPE_VACATION = 'VACATION';
+export const EVENT_TYPE_DUTY = 'DUTY';
+export const EVENT_TYPE_WARD = 'WARD';
+export const EVENT_TYPE_NO_DUTY_REQUEST = 'NO_DUTY_REQUEST';
+export const EVENT_TYPE_BLOCKED = 'BLOCKED';
 
 export const EventTypes = [
     { type: EVENT_TYPE_VACATION, name: 'Urlaub' },
     { type: EVENT_TYPE_DUTY, name: 'Dienst' },
     { type: EVENT_TYPE_WARD, name: 'Station' },
-    { type: EVENT_TYPE_MISC, name: 'Anderes' },
+    { type: EVENT_TYPE_NO_DUTY_REQUEST, name: 'Wunsch Dienstfrei' },
+    { type: EVENT_TYPE_BLOCKED, name: 'Geblockt' },
 ];
